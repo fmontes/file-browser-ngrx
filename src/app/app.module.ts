1ccf395403fb629816c98c3d8a922ffd70859372
Freddy Montes <freddymontes@gmail.com>
2018-12-28 07:33:16 -0600

    Add ngrx store and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+// not used in production
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+// this would be done dynamically with webpack for builds
+const environment = {
+  development: true,
+  production: false
+};
+
+export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, { metaReducers }),
+    EffectsModule.forRoot([]),
+    environment.development ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
