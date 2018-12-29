32d8faa262a142504435dd969703238e2c2f5409
Freddy Montes <freddymontes@gmail.com>
2018-12-29 15:19:19 -0600

    Simple renaming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to site-browser-ngrx!');
+    expect(page.getTitleText()).toEqual('Welcome to file-browser-ngrx!');
   });
 });
--- src/app/_main/app.component.spec.ts
+++ src/app/_main/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'site-browser-ngrx'`, () => {
+  it(`should have as title 'file-browser-ngrx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('site-browser-ngrx');
+    expect(app.title).toEqual('file-browser-ngrx');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to site-browser-ngrx!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to file-browser-ngrx!');
   });
 });
