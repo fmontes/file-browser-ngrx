5a336e92f82b9ebdb6c575dca4802f8196c04acf
Freddy Montes <freddymontes@gmail.com>
2018-12-30 16:36:57 -0600

    Add ChangeDetectionStrategy to the presentational components

--- src/app/file-browser/components/folder-content/folder-content.component.ts
+++ src/app/file-browser/components/folder-content/folder-content.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { TreeFileItem } from '../../models/tree-file-item.model';
 
 @Component({
   selector: 'app-folder-content',
   templateUrl: './folder-content.component.html',
-  styleUrls: ['./folder-content.component.scss']
+  styleUrls: ['./folder-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FolderContentComponent implements OnInit {
   @Input() data: TreeFileItem[];
--- src/app/file-browser/components/folder-tree/folder-tree.component.ts
+++ src/app/file-browser/components/folder-tree/folder-tree.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { FolderItem } from '../../models';
 
 @Component({
   selector: 'app-folder-tree',
   templateUrl: './folder-tree.component.html',
-  styleUrls: ['./folder-tree.component.scss']
+  styleUrls: ['./folder-tree.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FolderTreeComponent implements OnInit {
   @Input() data: FolderItem[];
